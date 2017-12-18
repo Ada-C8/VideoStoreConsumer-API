05ab3c1057d958f13e8dd31fd3d656db23469468
Lauren <Laurenelee88@gmail.com>
2017-12-18 11:19:08 -0800

    create keys for api

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,14 @@
 customer_data = JSON.parse(File.read('db/seeds/customers.json'))
 
 customer_data.each do |customer|
+  # puts customer
+
   Customer.create!(customer)
 end
 
 JSON.parse(File.read('db/seeds/movies.json')).each do |movie_data|
+  # puts movie_data
   movies = MovieWrapper.search(movie_data["title"])
   movies.first.save unless movies.empty?
+
 end
