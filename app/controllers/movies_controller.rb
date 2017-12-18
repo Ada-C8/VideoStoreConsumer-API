6b80282d65081e25b696ed0f7b7f4dad503aa639
Tanisha <tanishahampden@gmail.com>
2017-12-18 12:06:53 -0800

    add create controller action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,17 +11,30 @@ class MoviesController < ApplicationController
     render status: :ok, json: data
   end
 
+  def create
+    movie = Movie.new(movie_params)
+    if movie.save
+      render json: {id: movie.id}
+    else
+      render json: {errors: movie.errors.messages}, status: :bad_request
+    end
+  end
+
   def show
     render(
       status: :ok,
       json: @movie.as_json(
         only: [:title, :overview, :release_date, :inventory],
         methods: [:available_inventory]
-        )
       )
+    )
   end
 
   private
+
+  def movie_params
+    params.permit(:title, :overview, :inventory, :release_date)
+  end
 
   def require_movie
     @movie = Movie.find_by(title: params[:title])
