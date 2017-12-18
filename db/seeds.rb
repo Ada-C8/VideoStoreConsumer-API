b0be4ddd5cc3e2cf40bbded092aac60c886dbf34
averikitsch <aekitsch@gmail.com>
2017-12-18 11:38:53 -0800

    fixed inventory seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,7 @@ end
 
 JSON.parse(File.read('db/seeds/movies.json')).each do |movie_data|
   movies = MovieWrapper.search(movie_data["title"])
-  movies.first.save unless movies.empty?
+  movie = movies.first
+  movie.inventory = movie_data['inventory']
+  movie.save unless movies.empty?
 end
