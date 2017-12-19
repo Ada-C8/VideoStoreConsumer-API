7ddb07b0588f9913cf5941bbea7ed9f77fe43c97
Jessica Owens <vertige@gmail.com>
2017-12-19 11:04:59 -0800

    Commented out image url custom method in Movie.rb

--- lib/movie_wrapper.rb
+++ lib/movie_wrapper.rb
@@ -28,7 +28,7 @@ class MovieWrapper
       overview: api_result["overview"],
       release_date: api_result["release_date"],
       image_url: api_result["poster_path"] ? self.construct_image_url(api_result["poster_path"]) : nil,
-      #image_url: api_result["poster_path"], 
+      # image_url: api_result["poster_path"],
       external_id: api_result["id"])
   end
 
