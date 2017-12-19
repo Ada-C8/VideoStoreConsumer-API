3b3537c7c12e3e3f9d3b5cdbc2404b0770676da0
Bianca M Fernandez <bianca.maria.fernandez.22@gmail.com>
2017-12-19 11:54:49 -0800

    Add rack-cors gem to allow post. need to add create functionality on rails api

--- lib/movie_wrapper.rb
+++ lib/movie_wrapper.rb
@@ -8,7 +8,7 @@ class MovieWrapper
 
   def self.search(query)
     url = BASE_URL + "search/movie?api_key=" + KEY + "&query=" + query
-    # puts url
+    puts url
     response =  HTTParty.get(url)
     if response["total_results"] == 0
       return []
@@ -32,7 +32,7 @@ class MovieWrapper
   end
 
   def self.construct_image_url(img_name)
-    return BASE_IMG_URL + DEFAULT_IMG_SIZE + img_name
+    return img_name
   end
 
 end
