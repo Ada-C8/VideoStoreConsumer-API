4dccff873ac5f70d1a7b33d861fc544b102014e1
Jan Edrozo <jedrozo@gmail.com>
2017-12-20 11:13:56 -0800

    Movie_Wrapper.rb self.construct_image_url & self.construct_movie methods correction: Fixed duplicate img url building

--- lib/movie_wrapper.rb
+++ lib/movie_wrapper.rb
@@ -28,12 +28,16 @@ class MovieWrapper
       title: api_result["title"],
       overview: api_result["overview"],
       release_date: api_result["release_date"],
-      image_url: (api_result["poster_path"] ? self.construct_image_url(api_result["poster_path"]) : DEFAULT_IMG_URL ),
+      image_url: (api_result["poster_path"] ? self.construct_image_url(api_result["poster_path"]) : nil ),
       external_id: api_result["id"])
   end
 
   def self.construct_image_url(img_name)
-    return BASE_IMG_URL + DEFAULT_IMG_SIZE + img_name
+    if img_name.include?(BASE_IMG_URL)
+      return img_name
+    else
+      return BASE_IMG_URL + DEFAULT_IMG_SIZE + img_name
+    end
   end
 
 end
