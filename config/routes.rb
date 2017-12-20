e7b1a6686564729280fc00912349398e004bd8e8
Gale Harrington <gale.harrington19@gmail.com>
2017-12-20 13:03:37 -0800

    Add rack.cors gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  post "/movies", to: "movies#create"
+
   resources :customers, only: [:index]
 
   get "/movies/search", to: "movies#index"
