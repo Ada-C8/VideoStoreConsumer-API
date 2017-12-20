9147a2dd6a01e820e256ad77e19353bca7b9b57b
Tamira Vojnar <tamiravojnar@gmail.com>
2017-12-20 09:44:56 -0800

    added CORS functionality to api so I can make a post request from the front end. I added a gem and some code to the application class in application.rb

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,13 @@ module VideoStoreAPIRails
       'Access-Control-Allow-Origin' => 'http://localhost:8080',
       'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
     }
+
+    # add this in so I can make a post request 
+    config.middleware.insert_before 0, Rack::Cors do
+     allow do
+       origins '*'
+       resource '*', :headers => :any, :methods => [:get, :post, :options]
+     end
+   end
   end
 end
