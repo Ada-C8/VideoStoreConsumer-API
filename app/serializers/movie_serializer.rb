666b025537de7f3bde2f11e4c1cc7c2169d90b7e
Laura Robertson <mslaura.mae@gmail.com>
2017-12-20 13:06:27 -0800

    Adding serializer refrence to rating

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,5 +1,5 @@
 class MovieSerializer < ActiveModel::Serializer
   attribute :id, if: -> { object.id != nil }
 
-  attributes :title, :overview, :release_date, :average_rating, :image_url, :external_id
+  attributes :title, :overview, :release_date, :average_rating, :rating, :image_url, :external_id
 end
