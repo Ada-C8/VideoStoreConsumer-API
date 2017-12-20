8c2adaee32e578c4f6789d4f37b728d0334a5d1b
Sairagul Abdukhalieva <abdukhalieva@gmail.com>
2017-12-20 10:11:06 -0800

    Working on images

--- lib/movie_wrapper.rb
+++ lib/movie_wrapper.rb
@@ -22,17 +22,35 @@ class MovieWrapper
 
   private
 
-  def self.construct_movie(api_result)
-    Movie.new(
-      title: api_result["title"],
-      overview: api_result["overview"],
-      release_date: api_result["release_date"],
-      image_url: api_result["poster_path"], #(api_result["poster_path"] ? self.construct_image_url(api_result["poster_path"]) : nil),
-      external_id: api_result["id"])
-  end
+  # def self.construct_movie(api_result)
+  #   Movie.new(
+  #     title: api_result["title"],
+  #     overview: api_result["overview"],
+  #     release_date: api_result["release_date"],
+  #     image_url: api_result["poster_path"]?
+  #     self.construct_image_url(api_result["poster_path"]) : nil),
+  #     external_id: api_result["id"])
+  # end
+  #
+  # def self.construct_image_url(img_name)
+  #   return BASE_IMG_URL + DEFAULT_IMG_SIZE + img_name
+  # end
 
-  def self.construct_image_url(img_name)
-    return BASE_IMG_URL + DEFAULT_IMG_SIZE + img_name
-  end
+  def self.construct_movie(api_result)
+   Movie.new(
+     title: api_result["title"],
+     overview: api_result["overview"],
+     release_date: api_result["release_date"],
+     # image_url: api_result["poster_path"],
+     image_url: (api_result["poster_path"] ? self.construct_image_url(api_result["poster_path"]) : nil),
+     external_id: api_result["id"])
+ end
+
+ def self.construct_image_url(img_name)
+   puts BASE_IMG_URL + DEFAULT_IMG_SIZE + img_name
+   return BASE_IMG_URL + DEFAULT_IMG_SIZE + img_name
+ end
+
+
 
 end
